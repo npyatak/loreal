2577b9c75da921c9bfa851315e98d57decb9678a
Nick <pyatakn@gmail.com>
2018-03-01 19:51:52 +0300

    upd

--- frontend/views/site/video.php
+++ frontend/views/site/video.php
@@ -1,4 +1,6 @@
 <?php 
+use yii\helpers\Url;
+
 $this->params['bodyClass'] = 'page-video';
 ?>
 
@@ -75,7 +77,7 @@ $this->params['bodyClass'] = 'page-video';
 				<div class="sm1-img-mob"><img src="/images/s3__sm.png" alt=""></div>
 				<div class="sm1-body">Можно ли назвать тебя гуру макияжа и специалистом в области<br>современных косметических средств, или твоя женская суперсила<br>вовсе не в мэйкапе? Стоит ли подругам доверять твоим<br>рекомендациям? Давай узнаем!</div>
 				<div class="go-test">
-					<a href="#test" class="test-link">Пройти тест</a>
+					<a href="<?=Url::toRoute(['site/video', '#' => 'test']);?>" class="test-link">Пройти тест</a>
 				</div>
 			</div>
 			<div class="sm sm-2">
